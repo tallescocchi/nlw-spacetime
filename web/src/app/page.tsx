3a48dc4218d32afb2697d1cae2318ae5698045e7
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-05-15 23:59:25 -0300

    first commit

---
+++ web/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div
+      className="
+        h-screen
+        bg-zinc-950
+        p-6
+        text-zinc-100
+      "
+    >
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  )
+}
