30fc45fb423c70daa9105e6f505e5692a657782e
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-05-24 20:57:53 -0300

    Lession 4 finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import 'dotenv/config'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,6 +27,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
