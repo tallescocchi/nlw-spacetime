4f7cac6d0ead79274199e8023bc2f418983d8936
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-05-25 21:42:28 -0300

    Initial Lession 5

---
+++ web/src/components/NewMemoryForm.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Camera } from 'lucide-react'
+import { MediaPicker } from './MediaPicker'
+import { FormEvent } from 'react'
+
+export function NewMemoryForm() {
+  function handleCreateMemory(event: FormEvent) {
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleCreateMemory} className="flex flex-1 flex-col gap-2">
+      <div className="flex items-center gap-4">
+        <label
+          htmlFor="media"
+          className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+        >
+          <Camera className="h-4 w-4" />
+          Anexar mídia
+        </label>
+        <label
+          htmlFor="isPublic"
+          className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+        >
+          <input
+            type="checkbox"
+            name="isPublic"
+            id="isPublic"
+            value="true"
+            className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
+          />
+          Tornar memória pública
+        </label>
+      </div>
+
+      <MediaPicker />
+
+      <textarea
+        name="content"
+        spellCheck={false}
+        className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lemrar para sempre."
+      />
+    </form>
+  )
+}
+
+// 25:08
