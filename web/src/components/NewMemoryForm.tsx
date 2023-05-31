3a6dc6ad6eaaefe1f76672f7d680c024f0b48f26
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-05-31 20:50:08 -0300

    Lession 5 Finished

--- web/src/components/NewMemoryForm.tsx
+++ web/src/components/NewMemoryForm.tsx
@@ -3,10 +3,48 @@
 import { Camera } from 'lucide-react'
 import { MediaPicker } from './MediaPicker'
 import { FormEvent } from 'react'
+import { api } from '@/lib/api'
+import Cookie from 'js-cookie'
+import { useRouter } from 'next/navigation'
 
 export function NewMemoryForm() {
-  function handleCreateMemory(event: FormEvent) {
+  const router = useRouter()
+
+  async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    const fileToUpload = formData.get('coverUrl')
+
+    let coverUrl = ''
+
+    if (fileToUpload) {
+      const uploadFormData = new FormData()
+      uploadFormData.set('file', fileToUpload)
+
+      const uploadResponse = await api.post('/upload', uploadFormData)
+
+      coverUrl = uploadResponse.data.fileUrl
+    }
+
+    const token = Cookie.get('token')
+
+    await api.post(
+      '/memories',
+      {
+        coverUrl,
+        content: formData.get('content'),
+        isPublic: formData.get('isPublic'),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/')
   }
 
   return (
@@ -42,8 +80,13 @@ export function NewMemoryForm() {
         className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
         placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lemrar para sempre."
       />
+
+      <button
+        type="submit"
+        className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-semibold uppercase leading-none text-black hover:bg-green-600"
+      >
+        Salvar
+      </button>
     </form>
   )
 }
-
-// 25:08
